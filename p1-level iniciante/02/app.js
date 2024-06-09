6de1e0173262b9ca019d48191256c3bbf2ad00a7
maria vitoria alves santos <maviukii@gmail.com>
2024-06-09 16:19:43 -0300

    2

--- p1-level iniciante/02/app.js
+++
@@ -1,8 +0,0 @@
-const valorDaconta = Number(prompt("digite o valor da conta:"))
-const numeroDeclientes = Number(prompt("quantas pessoas comeram:"))
-const calculo = valorDaconta / numeroDeclientes
-
-alert(`valor da conta: ${valorDaconta}\n numero de clientes: ${numeroDeclientes}\n valor por cliente:  ${calculo}`)
-
-
-
